8e47cd45ec2e1e21e38cd5ace66e8f7510904b94
Maximilian Reichel <mr@phramz.com>
2013-04-29 22:00:18 +0200

    updated copy header

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,10 +14,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Phramz.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Phramz. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
  */
-
 
 $loader = require_once __DIR__ . "/../vendor/autoload.php";
 $loader->add('Phramz\\Commons\\', __DIR__);
